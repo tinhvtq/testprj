d64f472e475905721b0890b9ece3c2b189a45b57
tinhvtq <62053182+tinhvtq@users.noreply.github.com>
2020-06-16 10:57:26 +0900

    Create LogsController.cs

---
+++ Controllers/LogsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationSystem.Controllers
+{
+    public class LogsController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public LogsController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        public IActionResult ListImportLogs()
+        {
+            //check login
+            if (HttpContext.Session.GetInt32("UserType") == null)
+            {
+                return RedirectToAction("Login", "User");
+            }
+            string[] filePaths = Directory.GetFiles(_env.WebRootPath + "\\logs", "*.txt");
+
+            List<string> ListFiles = new List<string>();
+            // return Json(filePaths);
+
+            foreach (var dir in filePaths)
+            {
+                string[] items = dir.Split("\\");
+                ListFiles.Add(items[items.Length - 1]);
+            }
+            ViewBag.ListFiles = ListFiles;
+            //return Json(ListFiles);
+            return View();
+        }
+    }
+}
