df28049619aa5a7afb91136c6318697544702552
tinhvtq <62053182+tinhvtq@users.noreply.github.com>
2020-06-16 10:57:47 +0900

    Create Infomation.cs

---
+++ Models/Infomation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using ReservationSystem.Models;
+
+namespace ReservationSystem.Models
+{
+    public class Infomation
+    {
+        [Key]
+        public int InfomationId { get; set; }
+        public int UserId { get; set; }
+        public DateTime InfomationDate { get; set; }
+        public string Title { get; set; }
+        public string Note { get; set; }
+        public string InfomationFile { get; set; }
+        public virtual User User { get; set; }
+    }
+
+    public class ParamaterInfo
+    {
+        public int InfomationId { get; set; }
+        public int UserId { get; set; }
+        public DateTime InfomationDate { get; set; }
+        public string Title { get; set; }
+        public string Note { get; set; }
+        public string InfomationFile { get; set; }
+        public string InfomationFile_Old { get; set; }
+        public int StatusRemoveFile { get; set; } // 1 remove
+    }
+}
